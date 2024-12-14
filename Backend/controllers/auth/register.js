f0bea4f807f4944dfa4dbf4eb6a83e6ec55f503b
SHIVAM-KUMAR-59 <jsrshivamx7@gmail.com>
2024-12-14 17:18:44 +0530

    Lesssssgo

--- Backend/controllers/auth/register.js
+++ Backend/controllers/auth/register.js
@@ -1,10 +1,11 @@
+import jwt from 'jsonwebtoken'
 import User from '../../models/User'
 
 const loginUser = async (req, res) => {
-  const { email, password } = req.body
+  const { username, email, password } = req.body
 
-  if (!email || !password) {
-    return res.status(400).json({ message: 'Email and password are required' })
+  if (!email || !password || !username) {
+    return res.status(400).json({ message: 'Missing required fields' })
   }
 
   try {
@@ -12,8 +13,42 @@ const loginUser = async (req, res) => {
     if (existingUser) {
       return res.status(400).json({ message: 'User already exists' })
     }
+
+    const hashedPassword = await bcrypt.hash(password, 10)
+    const user = await User.create({
+      username,
+      email,
+      password: hashedPassword,
+    })
+
+    if (!user) {
+      return res.status(500).json({ message: 'Error creating user' })
+    }
+
+    await user.save()
+
+    const token = jwt.sign({ email }, process.env.JWT_SECRET, {
+      expiresIn: '1h',
+    })
+
+    if (!token) {
+      return res.status(500).json({ message: 'Error generating token' })
+    }
+
+    // Set token in the cookie
+    res.cookie('authToken', token, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === 'production',
+      maxAge: 3600000,
+    })
+
+    return res
+      .status(201)
+      .json({ message: 'User created successfully', data: user, token: token })
   } catch (error) {
     console.error(error)
     return res.status(500).json({ message: 'Internal server error' })
   }
 }
+
+export default loginUser
