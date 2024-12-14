333437b53cbf072cca9a98edd9850a9e895a03ef
SHIVAM-KUMAR-59 <jsrshivamx7@gmail.com>
2024-12-14 20:23:02 +0530

    Update Product Schema

--- Backend/models/Product.js
+++ Backend/models/Product.js
@@ -7,10 +7,8 @@ const Product = new mongoose.Schema({
   owner: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
   condition: { type: String, enum: ['New', 'Used'], required: true },
   category: { type: String, required: true },
-  quantity: { type: Number, default: 1 },
   barterRequirement: { type: String, required: false },
   barterCategory: { type: String, required: false },
-  barterQuantity: { type: Number, default: 0 },
   createdAt: { type: Date, default: Date.now },
 })
 
