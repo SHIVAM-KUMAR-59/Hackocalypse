56103906dd252db65b5b08f3d86814ab3d5d5acd
SHIVAM-KUMAR-59 <jsrshivamx7@gmail.com>
2024-12-14 19:39:25 +0530

    Get All Products Done

--- Backend/models/Product.js
+++ Backend/models/Product.js
@@ -2,6 +2,7 @@ import mongoose from 'mongoose'
 
 const Product = new mongoose.Schema({
   name: { type: String, required: true },
+  imageUrl: { type: String },
   description: { type: String, required: true },
   owner: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
   condition: { type: String, enum: ['New', 'Used'], required: true },
