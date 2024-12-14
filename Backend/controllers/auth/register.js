7a703132e8cdba8e53fad7f14d892244d6cc1d21
SHIVAM-KUMAR-59 <jsrshivamx7@gmail.com>
2024-12-14 18:24:35 +0530

    Register User Done

--- Backend/controllers/auth/register.js
+++ Backend/controllers/auth/register.js
@@ -15,7 +15,8 @@ const registerUser = async (req, res) => {
       return res.status(400).json({ message: 'User already exists' })
     }
 
-    const hashedPassword = await bcrypt.hash(password, 10)
+    const salt = await bcrypt.genSalt(10)
+    const hashedPassword = await bcrypt.hash(password, salt)
     const user = await User.create({
       username,
       email,
