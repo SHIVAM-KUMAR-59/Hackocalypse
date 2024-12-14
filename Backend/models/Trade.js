7776055444f04119fb1f600741e6448ce2c2773f
SHIVAM-KUMAR-59 <jsrshivamx7@gmail.com>
2024-12-14 21:09:58 +0530

    Trade Model Created

---
+++ Backend/models/Trade.js
@@ -0,0 +1,23 @@
+import mongoose from 'mongoose'
+
+const Trade = new mongoose.Schema({
+  user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+  offeredProduct: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Product',
+    required: true,
+  },
+  requestedProduct: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Product',
+    required: true,
+  },
+  status: {
+    type: String,
+    enum: ['pending', 'accepted', 'rejected', 'completed'],
+    default: 'pending',
+  },
+  createdAt: { type: Date, default: Date.now },
+})
+
+export default mongoose.model('Trade', Trade)
