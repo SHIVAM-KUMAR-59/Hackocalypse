18a436367071e5a23babe00009d30d03d9b290f8
SHIVAM-KUMAR-59 <jsrshivamx7@gmail.com>
2024-12-14 20:00:37 +0530

    Update Product Done

--- Backend/models/Product.js
+++ Backend/models/Product.js
@@ -2,12 +2,15 @@ import mongoose from 'mongoose'
 
 const Product = new mongoose.Schema({
   name: { type: String, required: true },
-  imageUrl: { type: String },
+  imageUrl: { type: String, required: false },
   description: { type: String, required: true },
   owner: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
   condition: { type: String, enum: ['New', 'Used'], required: true },
   category: { type: String, required: true },
   quantity: { type: Number, default: 1 },
+  barterRequirement: { type: String, required: false },
+  barterCategory: { type: String, required: false },
+  barterQuantity: { type: Number, default: 0 },
   createdAt: { type: Date, default: Date.now },
 })
 
